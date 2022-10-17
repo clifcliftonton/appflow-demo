f53e362dc0d68994aa244a59112d3832e3e1f313
Tim Lancina <tlancina@users.noreply.github.com>
2022-10-17 11:50:32 -0400

    expose environment var to sharedprefs

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,5 +1,21 @@
 package io.ionic.starter;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import io.ionic.starter.BuildConfig;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    if (!BuildConfig.TENANT_ID.equals("null")) {
+      SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+      SharedPreferences.Editor editor = sharedPref.edit();
+      editor.putString("TENANT_ID", BuildConfig.TENANT_ID);
+      editor.apply();
+    }
+  }
+}
